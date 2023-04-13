a3d4841032a98346407ce6452a04b37daf6e5cd4
osamadgfloat <123050370+osamadgfloat@users.noreply.github.com>
2023-04-13 12:59:58 +0500

    update

--- installTools.ps1
+++ installTools.ps1
@@ -2,5 +2,7 @@
 
 # $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; Remove-Item .\AzureCLI.msi
 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 
 choco install microsoft-build-tools
