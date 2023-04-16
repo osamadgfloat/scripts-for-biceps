d372bdf8b5d9218bc36a3eaf45620de4091779a9
osamadgfloat <osama.saeed@digifloat.com>
2023-04-17 02:37:46 +0500

    update

--- install.ps1
+++ install.ps1
@@ -2,13 +2,13 @@
 
 # $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; Remove-Item .\AzureCLI.msi
 
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -
-OutFile .\AzureCLI.msi
-Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi
-/quiet'
-Remove-Item .\AzureCLI.msi
+# Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -
+# OutFile .\AzureCLI.msi
+# Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi
+# /quiet'
+# Remove-Item .\AzureCLI.msi
 
-az upgrade
+# az upgrade
 
 winget install -e --id Microsoft.Bicep
 
